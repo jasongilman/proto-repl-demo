2cfe3c32e35ea3731d2f9de1c4c422224bc71a68
Fred Eisele <feisele@isis.vanderbilt.edu>
2016-03-08 09:12:45 -0600

    made the operation of autoeval.clj a bit more clear

--- autoeval.clj
+++ autoeval.clj
@@ -1,25 +1,30 @@
-(ns autoeval)
+(ns autoeval
+  (:require [prc :as prc]))
 
 ;; This file demonstrates the autoeval functionality.
 ;; All top level forms will be immediately evaluated.
 
-;; Turn on autoeval by bringing up the command palette and selecting Proto Repl: Autoeval File
+;; Turn on autoeval by bringing up the command palette
+;; and selecting Proto Repl: Autoeval File
 
 ;; Uncomment this line. It should be immediately evaluated
-; (+ 2 3)
+(comment
+ (+ 2 3))
 
 ;; Uncomment me
-; (for [n (range 10)]
-;   (* 2 n))
+(comment
+ (for [n (range 10)]
+   (* 2 n)))
 
 ;; Proto REPL Charts work as well if you have it installed.
 ;; See https://github.com/jasongilman/proto-repl-charts
 
 ;; Uncomment me
-; (prc/line-chart
-;  "Trigonometry"
-;  {"sin" (map #(Math/sin %) (range 0.0 6.0 0.2))
-;   "cos" (map #(Math/cos %) (range 0.0 6.0 0.2))})
+(comment
+ (prc/line-chart
+  "Trigonometry"
+  {"sin" (map #(Math/sin %) (range 0.0 6.0 0.2))
+   "cos" (map #(Math/cos %) (range 0.0 6.0 0.2))}))
 
 ;; Turn off Autoeval by bringing up the command palette and selecting:
 ;; Proto Repl: Stop Autoeval File
