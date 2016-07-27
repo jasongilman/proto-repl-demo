31a29001db66afd1855896132917c23a4d5e631b
Jason Gilman <jason@element84.com>
2016-07-26 21:23:11 -0400

    Updating to latest version of proto repl lib.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.3.0"]
+                 [proto-repl "0.3.1"]
                  [proto-repl-charts "0.3.1"]]
 
   :profiles
